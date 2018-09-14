4f9aa3a4a5475ff15dd74f3dede265cd199b5d7d
Hadeer Khalifa <ha.deeryehia@gmail.com>
2018-09-14 09:48:42 -0400

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/project/android/moviedb/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.project.android.moviedb;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.project.android.moviedb";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String API_KEY = "8934d5a9f489ef4ac9de9c6fb5aa6b7a";
+}
